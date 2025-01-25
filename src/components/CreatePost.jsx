f87fa83f53fddc24c999952303502df04bd2cb93
jinuscript <jinuscript@gmail.com>
2025-01-25 19:06:04 +0900

    FEAT: useMutation 훅을 활용해 POST 통신 수행

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,12 +1,26 @@
 import { useState } from "react";
+import { useMutation } from "@tanstack/react-query";
+
+const createPost = async (newPost) => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(newPost),
+  });
+
+  return response.json();
+};
 
 export default function CreatePost() {
   const [title, setTitle] = useState("");
+
+  const { mutate } = useMutation({ mutationFn: createPost });
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     // 데이터 뮤테이션
+    mutate({ title, body: "새로운 게시글입니다" });
   };
 
   return (
