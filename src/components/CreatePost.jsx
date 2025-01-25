9900d165116cb51339f2e0b3aebcf9fcfefa5409
jinuscript <jinuscript@gmail.com>
2025-01-25 19:38:32 +0900

    FEAT: onError를 활용해 업데이트 실패 시에 데이터 롤백 기능 추가

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -31,9 +31,9 @@ export default function CreatePost() {
 
       return { previousPosts };
     },
-    // onError: (err, newPost, context) => {
-    //   queryClient.setQueriesData(["posts"], context.previousPosts);
-    // },
+    onError: (err, newPost, context) => {
+      queryClient.setQueriesData(["posts"], context.previousPosts);
+    },
   });
 
   const handleSubmit = (e) => {
