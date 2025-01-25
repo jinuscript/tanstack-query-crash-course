2d8266c01351022f91d6bd6d7289f1666383341f
jinuscript <jinuscript@gmail.com>
2025-01-25 18:40:05 +0900

    FEAT: 게시글을 렌더링하는 Posts 컴포넌트 추가

---
+++ src/components/Posts.jsx
@@ -0,0 +1,34 @@
+import { useQuery } from "@tanstack/react-query";
+
+const fetchPosts = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+
+  if (!response.ok) throw new Error("Error fetching Posts");
+
+  return await response.json();
+};
+
+function Posts() {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["posts"],
+    queryFn: fetchPosts,
+  });
+
+  if (isLoading) {
+    return <p>로딩 중...</p>;
+  }
+
+  if (error) {
+    return <p>에러 발생: {error.message}</p>;
+  }
+
+  return (
+    <>
+      {data?.map((post) => (
+        <p key={post.id}>{post.title}</p>
+      ))}
+    </>
+  );
+}
+
+export default Posts;
